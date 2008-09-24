8a472f38d43eaa48a593499440f70a79a046bc93
Alexander Sheboltaev <a.sheboltaev@gmail.com>
2008-09-24 17:42:30 +0700

    Error dialog opening thread fixed.

--- com.yoursway.autoupdater.eclipse/src/com/yoursway/autoupdater/eclipse/UpdatableEclipseView.java
+++ com.yoursway.autoupdater.eclipse/src/com/yoursway/autoupdater/eclipse/UpdatableEclipseView.java
@@ -3,21 +3,27 @@ package com.yoursway.autoupdater.eclipse;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.internal.Workbench;
 
 import com.yoursway.autoupdater.auxiliary.AutoupdaterException;
 import com.yoursway.autoupdater.auxiliary.UpdatableApplicationView;
 
 public class UpdatableEclipseView implements UpdatableApplicationView {
     
-    public void displayAutoupdaterErrorMessage(AutoupdaterException e) {
-        Shell parent = Workbench.getInstance().getActiveWorkbenchWindow().getShell();
-        String dialogTitle = "Eclipse Autoupdater";
-        String message = "Cannot update Eclipse";
-        IStatus status = new Status(IStatus.ERROR, "com.yoursway.autoupdater.eclipse", multimessage(e));
-        
-        ErrorDialog.openError(parent, dialogTitle, message, status);
+    public void displayAutoupdaterErrorMessage(final AutoupdaterException e) {
+        Display.getDefault().asyncExec(new Runnable() {
+            public void run() {
+                //Shell parent = Workbench.getInstance().getActiveWorkbenchWindow().getShell();
+                Shell parent = null;
+                String dialogTitle = "Eclipse Autoupdater";
+                String message = "Cannot update Eclipse";
+                IStatus status = new Status(IStatus.ERROR, "com.yoursway.autoupdater.eclipse",
+                        multimessage(e));
+                
+                ErrorDialog.openError(parent, dialogTitle, message, status);
+            }
+        });
     }
     
     private String multimessage(AutoupdaterException e) {
