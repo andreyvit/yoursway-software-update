215727c3a50de8e40cb17c1ed0a9a61dd46106f7
Mikhail Kalugin <mikhail.kalugin@gmail.com>
2008-10-31 19:58:13 +0600

    Compilation error fix.

--- com.yoursway.tinyupdater/src/com/yoursway/tinyupdater/TinyUpdater.java
+++ com.yoursway.tinyupdater/src/com/yoursway/tinyupdater/TinyUpdater.java
@@ -54,7 +54,7 @@ public class TinyUpdater {
                 String releaseType = reader.readLine();
                 URL updateSite = new URL(reader.readLine());
                 
-                String envRelType = System.getenv(product.toUpperCase().replaceAll('-', '_') + "_RELTYPE");
+                String envRelType = System.getenv(product.toUpperCase().replaceAll("-", "_") + "_RELTYPE");
                 if (envRelType != null)
                     releaseType = envRelType;
                 
